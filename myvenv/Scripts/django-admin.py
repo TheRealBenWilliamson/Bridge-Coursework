e27c29524fd32dcaa5d57b5ff225aba468360868
TheRealBenWilliamson <mr.ben.williamson@gmail.com>
2020-06-07 15:00:08 +0100

    Bridge Coursework Setup

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\mrben\virtualenviroments\djangogirls\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
